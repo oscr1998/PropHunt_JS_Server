c5a8f4df3bc355870bb38cd088f6de091b6f922f
kaiyuleung <kyl1g16.ecs@gmail.com>
2022-12-08 13:22:15 +0000

    update lastest socket.io code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const { instrument } = require('@socket.io/admin-ui')
 const io = require('socket.io')(http, {
     cors: {
-        origin: ['https://admin.socket.io', 'http://localhost:3000', 'https://revelio.netlify.app'],
+        origin: ['https://admin.socket.io', 'http://localhost:3000' , 'https://revelio.netlify.app'],
         methods: ["GET", "POST"],
         credentials: true,
     }
@@ -25,61 +25,87 @@ const players = {
 io.on('connection', (socket) => {
     console.log(`player ${socket.id} connected`);
 
-    socket.on('join-room', (room, cb) => {
-        socket.join(room)
-        console.log(`player ${socket.id} joined room ${room}`);
-        cb({ id: socket.id, room: room })
-        if (!players[room]) {
-            players[room] = {}
+    socket.on('join-room', (room, isHost, gameInfo, cb) => {
+        
+        // if not host and room does not exist
+        if (!isHost && Object.keys(players).filter(r => r === room).length === 0){
+            cb({ id: socket.id, room: null })
+        } else {
+            if(isHost){
+                console.log(`Host ${socket.id} has created room ${room}`);
+                players[room] = {}
+                players[room][socket.id] = {
+                    id: socket.id,
+                    username: "",
+                    isHost: isHost,
+                    gameInfo: gameInfo,
+                }
+            } else {
+                console.log(`player ${socket.id} joined room ${room}`);
+                players[room][socket.id] = {
+                    id: socket.id,
+                    username: "",
+                    isHost: isHost,
+                    gameInfo: Object.values(players[room]).filter(p => p.isHost === true)[0].gameInfo
+                } 
+            }
+            socket.join(room)
+            cb({ id: socket.id, room: room })
+            io.to(room).emit('update-room', players[room])
         }
-        players[room][socket.id] = {
-            id: socket.id,
-            username: "",
-        }
-        io.to(room).emit('update-room', players[room])
     })
 
     socket.on('start-game', (room) => {
-        //
+        // Get game info
+        const mapIdx = players[room][socket.id].gameInfo.map 
+
         // Algorithm to decide who is the seeker
-        const listOfPlayers = Object.keys(players[room])    //["id1", "id2"]
+        const listOfPlayers = Object.keys(players[room])
         const seekerIdx = Math.floor(Math.random() * listOfPlayers.length)
         const seekerID = listOfPlayers[seekerIdx]
+        const listOfHiders = Object.keys(players[room]).filter(p => p !== seekerID)
+        const listOfSeekers = Object.keys(players[room]).filter(p => p === seekerID)
 
         //Algorithm to decide each players' spawn location
-        const coords = [
-            { x: 300, y: 300 },
-            { x: 400, y: 400 },
-            { x: 500, y: 500 },
+        const hiders_coords = [
+            [{ x: 880, y: 700 }, { x: 930, y: 700 }, { x: 980, y: 700 }, { x: 1030, y: 700 }],
+            [{ x: 1900, y: 320 }, { x: 1950, y: 320 }, { x: 2000, y: 320 }, { x: 2050, y: 320 }]
         ]
-        
-        listOfPlayers.forEach((id, idx) => {
-            if (id === seekerID) {
+
+        const seekers_coords = [
+            [{ x: 1800, y: 900 }, { x: 1800, y: 950 }],
+            [{ x: 300, y: 2000 }, { x: 320, y: 2000 }]
+        ]
+
+        listOfHiders.forEach((id, idx) => {
+            players[room][id] = {
+                ...players[room][id],
+                character: "hider",
+                velocity: 350,
+                isAlive: true,
+                x: hiders_coords[mapIdx-1][idx].x,
+                y: hiders_coords[mapIdx-1][idx].y,
+                propIndices: null
+            }
+        })
+
+        listOfSeekers.forEach((id, idx) => {
                 players[room][id] = {
                     ...players[room][id],
                     character: "seeker",
-                    velocity: 500,
+                    velocity: 425,
                     isAlive: true,
-                    x: 300,
-                    y: 2000,
+                    x: seekers_coords[mapIdx-1][idx].x,
+                    y: seekers_coords[mapIdx-1][idx].y,
                     propIndices: null
                 }
+            })
 
-            } else {
-                players[room][id] = {
-                    ...players[room][id],
-                    character: "hider",
-                    velocity: 350,
-                    isAlive: true,
-                    x: coords[idx].x,
-                    y: coords[idx].y,
-                    propIndices: null
-                }
-            }
-        })
         //Ignore
         io.to(room).emit('update-room', players[room])
         //Ignore
+
+        console.log(players[room]);
 
         io.to(room).emit('teleport-players')
 
@@ -97,9 +123,9 @@ io.on('connection', (socket) => {
 
     socket.on("endGame", (room, results) => {
         io.to(room).emit('results', results)
+        // delete players[room]
     })
     
-    //todo 
     socket.on("killed", (room, H_id) => {
         players[room][H_id].isAlive = false
         players[room][H_id].propIndices = null
@@ -111,10 +137,24 @@ io.on('connection', (socket) => {
         io.to(room).emit('update-client', players[room])
     })
 
+    //!
+    // socket.on('exit-all-rooms', () => {
+    //     Array.from(socket.rooms).forEach(
+    //         room => {
+    //             if (room !== socket.id && players[room]) {
+    //                 socket.leave(room)
+    //                 delete players[room][socket.id]
+    //                 io.to(room).emit('update-room', players[room])
+    //                 console.log(`player ${socket.id} left room ${room}`);
+    //             }
+    //         }
+    //     )
+    // })
+
     socket.on('disconnecting', () => {
         Array.from(socket.rooms).forEach(
             room => {
-                if (room !== socket.id) {
+                if (room !== socket.id && players[room]) {
                     delete players[room][socket.id]
                     io.to(room).emit('update-room', players[room])
                     console.log(`player ${socket.id} left room ${room}`);
